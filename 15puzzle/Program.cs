756527267df39aa1b54e225df00867013efde4d9
Vladislav Nekrasov <vladislav.nekrasov123@gmail.com>
2022-08-15 18:50:42 +0300

    additional code

--- 15puzzle/Program.cs
+++ 15puzzle/Program.cs
@@ -1,28 +1,33 @@
-﻿namespace Testing
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testing
 {
     class StateNode<T> : IComparable<StateNode<T>> where T : IComparable
     {
-        public double ValueF { get; set; }
+        public double F { get; set; }
         public T[,] State { get; set; }
-        public int EmptyCol { get; private set; }
+        public int EmptyColumn { get; private set; }
         public int EmptyRow { get; private set; }
-        public int ValueG { get; set; }
-        public string Representation { get; set; }
+        public int Depth { get; set; }
+        public string StringRepresentation { get; set; }
 
-        public StateNode(T[,] state, int emptyCol, int emptyRow, int valueG)
+        public StateNode() { }
+
+        public StateNode(T[,] state, int emptyColumn, int emptyRow, int depth)
         {
             if (state.GetLength(0) != state.GetLength(1))
                 throw new Exception("Number of columns and rows isn't the same!.");
 
-            State = state.Clone() as T[,];
-            EmptyCol = emptyCol;
+            State = (T[,]) state.Clone();
+            EmptyColumn = emptyColumn;
             EmptyRow = emptyRow;
-            ValueG = valueG;
+            Depth = depth;
             
             for(var i = 0; i < State.GetLength(0); i++)
             {
                 for (var j = 0; j < State.GetLength(1); j++)
-                    Representation += state[i, j] + ",";
+                    StringRepresentation += state[i, j] + ",";
             }
         }
 
@@ -41,16 +46,85 @@
             }
         }
 
-        public int CompareTo (StateNode<T> other)
+        public int CompareTo(StateNode<T> other)
         {
-            if (ValueF > other.ValueF)
+            if (F > other.F)
                 return 1;
-            if (ValueF < other.ValueF)
+            if (F < other.F)
                 return -1;
 
             return 0;
         }
     }
+
+    class AStar<T> where T : IComparable
+    {
+        public int StatesChecked { get; set; }
+        private readonly StateNode<T> SolutionState;
+        private T EmptyTile { get; set; }
+        private readonly PriorityQueue<StateNode<T>, double> queue;
+        private readonly HashSet<string> hash;
+
+        public AStar(StateNode<T> initial, StateNode<T> solution, T empty)
+        {
+            queue.Enqueue(initial, initial.F);
+            SolutionState = solution;
+            EmptyTile = empty;
+            hash = new HashSet<string>();
+        }
+
+        private double Heuristics(StateNode<T> node)
+        {
+            return 0;
+        }
+
+        private void ExpandNodes(StateNode<T> node)
+        {
+            T temporary;
+            T[,] newState;
+            var column = node.EmptyColumn;
+            var row = node.EmptyRow;
+            StateNode<T> newNode;
+
+            //going up
+            if (row > 0)
+            {
+                newState = (T[,])node.State.Clone();
+                temporary = newState[row - 1, column];
+                newState[row - 1, column] = EmptyTile;
+                newState[row, column] = temporary;
+                newNode = new StateNode<T>(newState, row - 1, column, node.Depth + 1);
+
+                if (!hash.Contains(newNode.StringRepresentation))
+                {
+                    newNode.F = node.Depth + Heuristics(newNode);
+                    queue.Enqueue(newNode, newNode.F);
+                    hash.Add(newNode.StringRepresentation);
+                }
+            }
+        }
+
+        public StateNode<T> Execute()
+        {
+            StateNode<T> dequeuedState = queue.Dequeue();
+            hash.Add(dequeuedState.StringRepresentation);
+
+            while(queue.Count > 0)
+            {
+                StatesChecked++;
+
+                if (dequeuedState.StringRepresentation.Equals(SolutionState.StringRepresentation))
+                    return dequeuedState;
+
+                ExpandNodes(dequeuedState);
+                dequeuedState = queue.Dequeue();
+            }
+
+            return null;
+        }
+    }
+
+
     class Program
     {
         static void Main()
